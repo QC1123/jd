d2efa1b1985047de756495c9e26c42391da25f14
kkcao <38519034+kkcao@users.noreply.github.com>
2021-01-19 05:35:25 +0800

    Update jdPlantBeanShareCodes.js

--- jdPlantBeanShareCodes.js
+++ jdPlantBeanShareCodes.js
@@ -7,7 +7,7 @@
 // 同一个京东账号的好友互助码用@符号隔开,不同京东账号之间用&符号或者换行隔开,下面给一个示例
 // 如: 京东账号1的shareCode1@京东账号1的shareCode2&京东账号2的shareCode1@京东账号2的shareCode2
 let PlantBeanShareCodes = [
-  '66j4yt3ebl5ierjljoszp7e4izzbzaqhi5k2unz2afwlyqsgnasq@olmijoxgmjutyrsovl2xalt2tbtfmg6sqldcb3q@e7lhibzb3zek27amgsvywffxx7hxgtzstrk2lba@olmijoxgmjutyx55upqaqxrblt7f3h26dgj2riy',//账号一的好友shareCode,不同好友中间用@符号隔开
+  'ybvc7vkuuqg3q3tc2h5pwmkfyi@e7lhibzb3zek2o3osubib6jnwdifwbxlqmpzu7y@haofcpgvxtwwoksczndwypn7d7qcxytdwhsfwpa@awus372yrusdniyfhgunn4m554@oss2ycictwzowutap6w6fvllhfnzpbfw4uapnvi@4npkonnsy7xi2shhdpwzao2adev6aemttkh3coq@e7lhibzb3zek33girhyhdliwt5fg7y7coc6fbcq@66nvo67oyxpyce6odctijj3nrhl5wdost2w3fry',//账号一的好友shareCode,不同好友中间用@符号隔开
   'mlrdw3aw26j3wgzjipsxgonaoyr2evrdsifsziy@mlrdw3aw26j3wgzjipsxgonaoyr2evrdsifsziy',//账号二的好友shareCode，不同好友中间用@符号隔开
 ]
 // 判断github action里面是否有种豆得豆互助码
