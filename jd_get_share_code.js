4995a1d108616bec62ca671201a06a3c451d7c8b
kkcao <38519034+kkcao@users.noreply.github.com>
2021-01-20 09:49:32 +0800

    Update jd_get_share_code.js

--- jd_get_share_code.js
+++ jd_get_share_code.js
@@ -22,6 +22,7 @@ cron "20 13 * * 6" script-path=https://raw.githubusercontent.com/LXK9301/jd_scri
  */
 const $ = new Env("获取互助码");
 const JD_API_HOST = "https://api.m.jd.com/client.action";
+const notify = $.isNode() ? require('./sendNotify') : '';
 let cookiesArr = [], cookie = '', message;
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 !function (n) {
