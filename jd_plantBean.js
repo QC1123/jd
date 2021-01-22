2e7ce2fc9052e0179b45d2df0365e4656de8413f
kkcao <38519034+kkcao@users.noreply.github.com>
2021-01-22 12:58:59 +0800

    Update jd_plantBean.js

--- jd_plantBean.js
+++ jd_plantBean.js
@@ -34,9 +34,9 @@ const JD_API_HOST = 'https://api.m.jd.com/client.action';
 //下面给出两个账号的填写示例（iOS只支持2个京东账号）
 let shareCodes = [ // IOS本地脚本用户这个列表填入你要助力的好友的shareCode
                    //账号一的好友shareCode,不同好友的shareCode中间用@符号隔开
-  '66j4yt3ebl5ierjljoszp7e4izzbzaqhi5k2unz2afwlyqsgnasq@olmijoxgmjutyrsovl2xalt2tbtfmg6sqldcb3q@e7lhibzb3zek27amgsvywffxx7hxgtzstrk2lba@e7lhibzb3zek32e72n4xesxmgc2m76eju62zk3y',
+  'ybvc7vkuuqg3q3tc2h5pwmkfyi@haofcpgvxtwwoksczndwypn7d7qcxytdwhsfwpa@e7lhibzb3zek2o3osubib6jnwdifwbxlqmpzu7y@awus372yrusdniyfhgunn4m554@oss2ycictwzowutap6w6fvllhfnzpbfw4uapnvi@4npkonnsy7xi2shhdpwzao2adev6aemttkh3coq@53c3u54x45cxpg6jra5vmwmmyq@e7lhibzb3zek33girhyhdliwt5fg7y7coc6fbcq@66nvo67oyxpyce6odctijj3nrhl5wdost2w3fry',
   //账号二的好友shareCode,不同好友的shareCode中间用@符号隔开
-  'olmijoxgmjutyx55upqaqxrblt7f3h26dgj2riy@mlrdw3aw26j3wgzjipsxgonaoyr2evrdsifsziyvnsb2r54jq34s64sc4it3jlfnejwmtmsuadax2i@eeexxudqtlampbpvmceutaaht5tcftvr6kohuny',
+  'ybvc7vkuuqg3q3tc2h5pwmkfyi@haofcpgvxtwwoksczndwypn7d7qcxytdwhsfwpa@e7lhibzb3zek2o3osubib6jnwdifwbxlqmpzu7y@awus372yrusdniyfhgunn4m554@oss2ycictwzowutap6w6fvllhfnzpbfw4uapnvi@4npkonnsy7xi2shhdpwzao2adev6aemttkh3coq@53c3u54x45cxpg6jra5vmwmmyq@e7lhibzb3zek33girhyhdliwt5fg7y7coc6fbcq@66nvo67oyxpyce6odctijj3nrhl5wdost2w3fry',
 ]
 let currentRoundId = null;//本期活动id
 let lastRoundId = null;//上期id
